81d56b7fbdd34bbffd159c15819d14d098290cad
Noonwraith <janmpetersen@posteo.de>
2023-06-27 18:55:45 +0200

    aesthetic changes

--- src/Debuging/JsonHandler.java
+++ src/Debuging/JsonHandler.java
@@ -3,7 +3,6 @@ package Debuging;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
